1d51aecfb21903b2f1ba160be5d148292c653794
ngockhoi96 <anicedfaant@gmail.com>
2024-03-27 17:56:11 +0700

    update tab indicator

--- src/components/tabs/tab-indicator.tsx
+++ src/components/tabs/tab-indicator.tsx
@@ -1,6 +1,6 @@
 import cx from 'clsx';
 
-import { useLayoutEffect, useState } from 'react';
+import { useEffect, useLayoutEffect, useRef, useState } from 'react';
 import { useTabsContext } from './tabs.context';
 import './tabs.style.css';
 
@@ -10,14 +10,12 @@ type TabIndicatorRect = {
 };
 
 function TabIndicator() {
-  const [rect, setRect] = useState<TabIndicatorRect>({
-    left: 0,
-    width: 0,
-  });
+  const indicatorRef = useRef<HTMLSpanElement>(null);
 
   const [hasMeasured, setHasMeasured] = useState<boolean>(false);
 
   const { activeValue, rootRef } = useTabsContext();
+  const isFirstRender = useRef(true);
 
   //* useEffect runs asynchronously and after a render is painted to the screen.
   //* You cause a visual break if your effect changes something in the DOM
@@ -29,37 +27,50 @@ function TabIndicator() {
 
   //todo useLayoutEffect instead of useEffect to prevent visual break
   useLayoutEffect(() => {
-    if (!rootRef.current) return;
+    if (!rootRef.current || !indicatorRef.current) return;
 
     const currentActiveTab = rootRef.current.ownerDocument.querySelector(
       `[data-index="${activeValue}"]`
     ) as HTMLButtonElement;
 
-    setRect({
-      left: currentActiveTab.offsetLeft,
-      width: currentActiveTab.offsetWidth,
-    });
+    indicatorRef.current.style.left = `${currentActiveTab.offsetLeft}px`;
+    indicatorRef.current.style.width = `${currentActiveTab.offsetWidth}px`;
 
-    //* The requestAnimationFrame function is used to delay the setting of hasMeasured to true
-    //* until the next repaint.This is done to prevent an unwanted transition
-    //* from 0 to the measured rect.
-    const rafID = requestAnimationFrame(() => {
-      setHasMeasured(true);
-    });
+    if (isFirstRender.current) {
+      indicatorRef.current.style.transition = 'none';
+      isFirstRender.current = false;
+    } else
+      indicatorRef.current.style.transition = `left 0.4s cubic-bezier(0, 0.2, 0.4, 1.1),
+    width 0.4s cubic-bezier(0, 0.2, 0.4, 1.1)`;
 
-    return () => {
-      if (rafID) cancelAnimationFrame(rafID);
-    };
+    // setRect({
+    //   left: currentActiveTab.offsetLeft,
+    //   width: currentActiveTab.offsetWidth,
+    // });
+
+    // //* The requestAnimationFrame function is used to delay the setting of hasMeasured to true
+    // //* until the next repaint.This is done to prevent an unwanted transition
+    // //* from 0 to the measured rect.
+    // const rafID = requestAnimationFrame(() => {
+    //   setHasMeasured(true);
+    // });
+
+    // return () => {
+    //   if (rafID) cancelAnimationFrame(rafID);
+    // };
   }, [activeValue, rootRef]);
+
+  console.count('TabIndicator');
 
   return (
     <span
+      ref={indicatorRef}
       className={cx('tab-indicator')}
-      style={{
-        left: rect.left,
-        width: rect.width,
-        transitionDuration: hasMeasured ? '0.4s' : '0s',
-      }}
+      // style={{
+      //   left: rect.left,
+      //   width: rect.width,
+      //   transitionDuration: hasMeasured ? '0.4s' : '0s',
+      // }}
     ></span>
   );
 }
